069cfce42cfeda4802bb9e5a809d1a800dcca1d8
runikdiv <runikdiv@DESKTOP-KQE1F9L>
2021-10-10 23:08:38 +0300

    Добавьте файлы проекта.

---
+++ Module 1_Exercise 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_1_Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день! Введите целое число (N > 0)");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                sum = sum + (2 * i - 1);
+                Console.WriteLine("Текущее значение суммы: {0}", sum);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Квадрат искомого числа: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
